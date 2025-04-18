ab246278b51cef36ba5d40d61b72febc9c05b24e
Jan Zenisek <janzenisek@gmx.at>
2025-04-18 14:50:32 +0200

    added build and deploy cmds

--- src/Seidlr/Repl.cs
+++ src/Seidlr/Repl.cs
@@ -5,11 +5,13 @@ using Ai.Hgb.Seidl.Data;
 using Ai.Hgb.Seidl.Processor;
 using Docker.DotNet;
 using Docker.DotNet.Models;
+using Microsoft.CodeAnalysis;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
 using System.Net.Http.Json;
 using System.Reflection;
+using System.Reflection.Emit;
 using System.Runtime.Intrinsics.X86;
 using System.Text;
 using System.Text.Json;
@@ -78,7 +80,7 @@ namespace Ai.Hgb.Runtime {
     #endregion properties
 
     #region fields
-    private static string[] COMMANDS = { "run", "demo", "state", "start", "pause", "stop", "attach", "detach", "cancel", "save", "exit", "q", "quit", "list", "monitor", "generate", "stubs", "desc" };
+    private static string[] COMMANDS = { "run", "demo", "state", "startup", "pause", "stop", "attach", "detach", "cancel", "save", "exit", "q", "quit", "list", "monitor", "generate", "stubs", "desc", "build", "deploy" };
     private static string DOCKER_RUNTIME_ID_PREFIX = "ai.hgb.runtime";
     private static string DOCKER_APPLICATION_ID_PREFIX = "ai.hgb.application";
 
@@ -100,6 +102,10 @@ namespace Ai.Hgb.Runtime {
     private string runningTask;
     private BrokerConfiguration brokerConfigBase;
     private SocketConfiguration socketConfigBase;
+    private bool booted;
+    private string descriptionPath;
+    private ScopedSymbolTable sst;
+    private List<Generator.ProjectInfo> projects;
     #endregion fields
 
     public Repl() {
@@ -114,6 +120,8 @@ namespace Ai.Hgb.Runtime {
       //activeProcesses = new List<Process>();
       activeContainers = new Dictionary<string, List<CreateContainerResponse>>();
       activeProcesses = new Dictionary<string, List<Process>>();
+      descriptionPath = null;
+      booted = false;
 
       Startup = new List<RuntimeComponent>();
       foreach (var i in config.Startup) {
@@ -155,8 +163,6 @@ namespace Ai.Hgb.Runtime {
         try {
           if (arguments == null || arguments.Length == 0) {
             Console.WriteLine("seidlr v0.0.1-alpha\n");
-            Console.WriteLine("> starting up...");
-            StartupRuntime();
 
             do {
               Console.Write("> ");
@@ -180,16 +186,16 @@ namespace Ai.Hgb.Runtime {
 
     private Dictionary<string, List<string>> ParseLine(string[] arguments) {
       var commands = new Dictionary<string, List<string>>();
+      string curCmd = "";      
 
-      string curCmd = "";
       for (int i = 0; i < arguments.Length; i++) {
         var arg = arguments[i].Trim();
         if (COMMANDS.Contains(arg)) {
           commands.Add(arg, new List<string>());
-          curCmd = arg;
+          curCmd = arg;          
         }
         else if (!string.IsNullOrEmpty(curCmd)) {
-          commands[curCmd].Add(arg);
+          commands[curCmd].Add(arg);          
         }
       }
 
@@ -201,44 +207,56 @@ namespace Ai.Hgb.Runtime {
         if (cmd.Key == "exit" || cmd.Key == "q" || cmd.Key == "quit") {
           exit = true;
         }
+        else if(cmd.Key == "startup") {          
+          StartupRuntime();
+        }
         else if (cmd.Key == "run") {
           string path = null;
           bool foundFile = false;
           int optionsCounter = 0;
           string runtime = "docker";
 
-          if(cmd.Value != null && cmd.Value.Count > 0) {
+          if (cmd.Value != null && cmd.Value.Count > 0) {
             if (cmd.Value[optionsCounter] == "docker") optionsCounter++;
             if (cmd.Value[optionsCounter] == "process") { runtime = "process"; optionsCounter++; }
           }
 
           if (cmd.Value != null && cmd.Value.Count > 0) {
             path = cmd.Value[optionsCounter];
-            if(!path.EndsWith(".3l")) path = path + ".3l";
+            if (!path.EndsWith(".3l")) path = path + ".3l";
             if (File.Exists(path)) foundFile = true;
-            if (!foundFile) path = Path.Combine(@"..\..\..\..\DemoApps\SeidlTexts\", path);
+            if (!foundFile) path = Path.Combine(@"..\..\..\..\DemoApps\Texts\", path);
             if (File.Exists(path)) foundFile = true;
           }
 
           if (foundFile) {
-            if(runtime == "docker") PerformRunSeidl_Docker(path).Wait();
+            if (runtime == "docker") PerformRunSeidl_Docker(path).Wait();
             else if (runtime == "process") PerformRunSeidl_Process(path).Wait();
             runningTask = Guid.NewGuid().ToString();
           }
         }
-        else if (cmd.Key == "generate") {
-          if(cmd.Value != null && cmd.Value.Count == 3) {
-            string mode = cmd.Value[0];
-            string descPath = cmd.Value[1];
-            string stubsPath = cmd.Value[2];
+        else if (cmd.Key == "generate") {          
+          bool foundFile = false;
 
-            if(mode == "stubs") {
-              GenerateStubs(descPath, stubsPath);
-            } else {
-              Console.WriteLine("For description text generation, please use the utility function in Ai.Hgb.Seidl\n");
-            }
-
+          if (cmd.Value != null && cmd.Value.Count == 2) {
+            descriptionPath = cmd.Value[0];
+            if (!descriptionPath.EndsWith(".3l")) descriptionPath = descriptionPath + ".3l";
+            if (File.Exists(descriptionPath)) foundFile = true;
+            if (!foundFile) descriptionPath = Path.Combine(@"..\..\..\..\DemoApps\Texts\", descriptionPath);
+            if (File.Exists(descriptionPath)) foundFile = true;
+            
+            string stubsPath = cmd.Value[1];
+            this.projects = GenerateSolution(descriptionPath, stubsPath);
           }
+        }
+        else if (cmd.Key == "build") {
+          Console.WriteLine("building...");
+
+          
+          
+        }
+        else if (cmd.Key == "deploy") {
+          Console.WriteLine("deploying...");
         }
         else if (cmd.Key == "demo") {
           PerformDemo().Wait();
@@ -290,9 +308,13 @@ namespace Ai.Hgb.Runtime {
       if (Startup.Contains(RuntimeComponent.Repository) && Startup.Contains(RuntimeComponent.LanguageService)) {
         InitRepository();
       }
+
+      booted = true;
     }
 
     private void TeardownRuntime() {
+      if (!booted) return;
+
       PerformStop(); // teardown all started processes and containers
 
       var teardown = Startup.ToList();
@@ -511,7 +533,15 @@ namespace Ai.Hgb.Runtime {
         }
       }, cts.Token);
     }
-    #endregion runtime~
+
+    private List<Generator.ProjectInfo> GenerateSolution(string sourcePath, string targetDir) {
+      sst = ReadSeidl(sourcePath);
+      var generator = new Generator();
+      var projects = generator.GenerateSolution(sst, targetDir, true);
+      return projects;
+
+    }
+    #endregion runtime
 
     #region commands
 
@@ -608,6 +638,8 @@ namespace Ai.Hgb.Runtime {
 
     private Task PerformStop(string key = null) {
       return Task.Run(async () => {
+        if (!booted) return;
+
         List<Process> killedProcesses = null;
         List<CreateContainerResponse> killedContainers = null;
 
@@ -814,256 +846,6 @@ namespace Ai.Hgb.Runtime {
       }, cts.Token);
     }
 
-    private Task GenerateStubs(string path, string resultPath) {
-      return Task.Run(async () => {
-        try {
-          string runId = Guid.NewGuid().ToString();
-          string text = File.ReadAllText(path);
-          string fileName = Path.GetFileNameWithoutExtension(path);
-          var pr = new ProgramRecord(text);
-
-          string name = fileName;
-          string scope = "Sample";
-
-          var sb = new StringBuilder();
-
-          RoutingTable rt = null;
-          List<InitializationRecord> inits = null;
-
-          var routingResponse = await languageServiceClient.PostAsJsonAsync("translate/routing", pr);
-          if (routingResponse.IsSuccessStatusCode) {
-            rt = await routingResponse.Content.ReadFromJsonAsync<RoutingTable>();
-          } else {
-            throw new HttpRequestException();
-          }
-
-          var initResponse = await languageServiceClient.PostAsJsonAsync("translate/initializations", pr);
-          if (initResponse.IsSuccessStatusCode) {
-            inits = await initResponse.Content.ReadFromJsonAsync<List<InitializationRecord>>();
-          } else {
-            throw new HttpRequestException();
-          }
-
-          sb.AppendLine(
-            $$"""
-            using System.Text.Json;
-            using System.Text.Json.Serialization;
-            using Ai.Hgb.Common.Entities;
-            using Ai.Hgb.Dat.Communication;
-            using Ai.Hgb.Dat.Configuration;
-            
-            namespace Ai.Hgb.Application.{{scope}}.{{name}} {
-              
-              public class Parameters : IApplicationParametersBase, IApplicationParametersNetworking {
-                [JsonPropertyName("name")]
-                public string Name { get; set; }
-
-                [JsonPropertyName("description")]
-                public string Description { get; set; }
-
-                [JsonPropertyName("applicationParametersBase")]
-                public ApplicationParametersBase ApplicationParametersBase { get; set; }
-
-                [JsonPropertyName("applicationParametersNetworking")]
-                public ApplicationParametersNetworking ApplicationParametersNetworking { get; set; }
-              }
-
-              public class Program {
-                public static void Main(string[] args) {
-                  Console.WriteLine("{{scope}}.{{name}}\n");
-                  var parameters = JsonSerializer.Deserialize<Parameters>(args[0]);
-                  var routingTable = JsonSerializer.Deserialize<RoutingTable>(args[1]);
-
-                  // setup socket and converter
-                  var address = new HostAddress(parameters.ApplicationParametersNetworking.HostName, parameters.ApplicationParametersNetworking.HostPort);
-                  var converter = new JsonPayloadConverter();
-                  var cts = new CancellationTokenSource();
-                  ISocket socket = null;
-
-                  try {
-                    socket = new MqttSocket(parameters.Name, parameters.Name, address, converter, connect: true);
-            """);
-
-
-          foreach (var init in inits) {
-            foreach (var route in rt.Routes.Where(x => x.Sink.Id == init.name)) {
-              sb.AppendLine($"socket.Subscribe<>({route.SinkPort.Address}, ProcessPayload, cts.Token);");
-            }
-            foreach (var route in rt.Routes.Where(x => x.Source.Id == init.name)) {
-              sb.AppendLine($"socket.Publish({route.SourcePort.Address}, new Payload(...));");
-            }
-          }
-
-          sb.AppendLine(
-            $$"""              
-                  } catch(Exception ex) {
-                    Console.WriteLine(ex.Message);
-                  } finally {
-                    socket.Disconnect();
-                  }
-                }
-              }
-            }            
-            """);
-
-          File.WriteAllText(resultPath, sb.ToString());    
-          
-        } catch (Exception ex) { Console.WriteLine(ex.Message); }
-      }, cts.Token);
-    }
-
-    private Task GenerateProjectFile(string sourcePath, string resultPath, string scope, string name) {
-      return Task.Run(async () => {
-        try {
-          var sb = new StringBuilder();
-
-          sb.AppendLine(
-            $$"""
-              <Project Sdk="Microsoft.NET.Sdk">
-
-              <PropertyGroup>
-                <OutputType>Exe</OutputType>
-                <TargetFramework>net9.0</TargetFramework>
-                <ImplicitUsings>enable</ImplicitUsings>
-                <Nullable>enable</Nullable>
-                <AssemblyName>Ai.Hgb.Application.{{scope}}.$(MSBuildProjectName)</AssemblyName>
-                <RootNamespace>Ai.Hgb.Application.{{scope}}.$(MSBuildProjectName.Replace(" ", "_"))</RootNamespace>
-              </PropertyGroup>
-
-            	<ItemGroup>
-            	  <PackageReference Include="Ai.Hgb.Common.Entities" Version="0.1.13-prerelease-gb162266326" />
-            	  <PackageReference Include="Ai.Hgb.Dat.Communication" Version="0.1.7-prerelease-gdaf7bfe3e4" />
-            	  <PackageReference Include="Ai.Hgb.Dat.Configuration" Version="0.1.11-prerelease-gdaf7bfe3e4" />
-            	  <PackageReference Include="Ai.Hgb.Dat.Utils" Version="0.1.5-prerelease-gdaf7bfe3e4" />
-            	</ItemGroup>
-            	<ItemGroup>
-            	  <ProjectReference Include="..\Common\Common.csproj" />
-            	</ItemGroup>
-
-            </Project>
-            
-            """);
-
-          File.WriteAllText(resultPath, sb.ToString());
-        }
-        catch (Exception ex) { Console.WriteLine(ex.Message); }
-      }, cts.Token);
-    }
-
-    private Task GenerateDockerFile(string sourcePath, string resultPath, string scope, string name) {
-      return Task.Run(async () => {
-        try {
-          var sb = new StringBuilder();
-
-          sb.AppendLine(
-            $$"""
-              FROM mcr.microsoft.com/dotnet/sdk:9.0 AS build-env
-              WORKDIR .
-
-              # Copy everything
-              COPY "{{name}}/*.csproj" "{{name}}/"
-              COPY "Common/*.csproj" "Common/"
-
-              # Restore as distinct layers
-              RUN dotnet restore "{{name}}/{{name}}.csproj"
-
-              # Build and publish a release
-              COPY "{{name}}/" "{{name}}/"
-              COPY "Common/" "Common/"
-              WORKDIR /{{name}}
-              RUN dotnet publish -c Release -o out
-              COPY {{name}}/configurations /{{name}}/out/configurations
-
-              # Build runtime image
-              FROM mcr.microsoft.com/dotnet/aspnet:9.0
-              WORKDIR /{{name}}
-              COPY --from=build-env /{{name}}/out .
-              ENTRYPOINT ["dotnet", "Ai.Hgb.Application.{{scope}}.{{name}}.dll"]
-            </Project>
-            
-            """);
-
-          File.WriteAllText(resultPath, sb.ToString());
-        }
-        catch (Exception ex) { Console.WriteLine(ex.Message); }
-      }, cts.Token);
-    }
-
-    private Task GenerateCommonProject(string resultPath, string scope, string name) {
-      return Task.Run(async () => {
-      try {
-
-          // Data.cs
-          var sb = new StringBuilder();
-
-          sb.AppendLine(
-          $$"""
-            namespace Ai.Hgb.Application.{{scope}}.Common {
-            public struct Message {
-              public string Id { get; set; }
-              public string Text { get; set; }
-              public double Value { get; set; }
-
-              public Document(string id, string text, double value) {
-                Id = id;
-                Author = author;
-                Value = value;
-              }
-
-              public override string ToString() {
-                return $"Id: {Id}\tText: {Text}\tValue: {Value}";
-              }
-            }
-          }          
-          """);
-      File.WriteAllText(resultPath, sb.ToString());
-      }
-        catch (Exception ex) { Console.WriteLine(ex.Message); }
-      }, cts.Token);
-    }
-     
-    
-    record ProjectInfo (string pguid, string name, string path, string tguid);
-    private Task GenerateSolutionFile(string resultPath, string name, List<ProjectInfo> projects) {
-      return Task.Run(async () => {
-        try {
-          var sb = new StringBuilder();
-
-          sb.AppendLine(
-            $$"""
-              Microsoft Visual Studio Solution File, Format Version 12.00
-              # Visual Studio Version 17
-              VisualStudioVersion = 17.4.33205.214
-              MinimumVisualStudioVersion = 10.0.40219.1
-            """);
-
-          // Lib: 9A19103F-16F7-4668-BE54-9A1E7A4F7556
-          // App: FAE04EC0-301F-11D3-BF4B-00C04F79EFBC
-          foreach (var project in projects) {
-            sb.AppendLine(
-              $$"""
-                Project("{{{project.tguid}}}") = "{{project.name}}", "{{project.path}}", "{{{project.pguid}}}"
-              """);
-          }
-
-          sb.AppendLine(
-            $$"""
-              Global
-                GlobalSection(SolutionConfigurationPlatforms) = preSolution
-            	    Debug|Any CPU = Debug|Any CPU
-            	    Release|Any CPU = Release|Any CPU
-                EndGlobalSection
-                GlobalSection(SolutionProperties) = preSolution
-            	    HideSolutionNode = FALSE
-                EndGlobalSection
-              EndGlobal
-            """);
-
-          File.WriteAllText(resultPath, sb.ToString());
-        } catch (Exception ex) { Console.WriteLine(ex.Message); }
-      }, cts.Token);
-    }
-
     #endregion commands
 
     #region helper
@@ -1085,6 +867,12 @@ namespace Ai.Hgb.Runtime {
     }
 
     #endregion helper
+
+    #region code generation
+
+
+
+    #endregion code generation
 
     #region sandbox
     private Task PerformAttachBroker() {
@@ -1137,10 +925,10 @@ namespace Ai.Hgb.Runtime {
       if (string.IsNullOrEmpty(path)) path = @"..\..\..\..\DemoApps\SeidlTexts\main.3l";
       string text = File.ReadAllText(path);
       var parser = Utils.TokenizeAndParse(text);
-      var linter = new Linter(parser);
-      linter.ProgramTextUrl = path;
+      var transformer = new Transformer(parser);
+      transformer.ProgramTextUrl = path;
 
-      return linter.CreateScopedSymbolTableSecured();
+      return transformer.CreateScopedSymbolTableSecured();
     }
 
 
